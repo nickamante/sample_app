671d20325591c27c1ccb058cb8705dfa0cb85ab3
Nick Amante <namante@gmail.com>
2012-04-24 20:16:41 -0400

    Update routes and links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,54 +5,54 @@ describe "StaticPages" do
 
   describe "Home Page" do
   	it "should have the the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "#{base_title}")
     end
 
     it "should not have a custom page title" do 
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => "| Home")
     end
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
   end
 
   describe "Help Page" do
 	it "should have the the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => "#{base_title} | Help")
     end
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
   end
 
   describe "About Page" do
   	it "should have the the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => "#{base_title} | About Us")
     end
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
   end
 
   describe "Contact Page" do
   	it "should have the right title" do
-  	  visit '/static_pages/contact'
-  	  page.should have_selector('title', :text => "#{base_title} | Contact")
+  	  visit contact_path
+  	  page.should have_selector('title', text: "#{base_title} | Contact")
   	end
 
   	it "should have the h1 'Contact'" do
-  	  visit '/static_pages/contact'
-  	  page.should have_selector('h1', :text => 'Contact')
+  	  visit contact_path
+  	  page.should have_selector('h1', text: 'Contact')
   	end
   end
 end
