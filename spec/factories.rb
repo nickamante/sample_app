476b5b7c900c0355688b9d8ec5e298c24fd8f667
Nick Amante <namante@gmail.com>
2012-05-04 17:05:47 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+  	name "Nicholas Amante"
+  	email "namante@gmail.com"
+  	password "foobar"
+  	password_confirmation "foobar"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,10 +2,44 @@ require 'spec_helper'
 
 describe "UserPages" do
   subject { page }
-  before { visit signup_path }
-
+  
   describe "Signup Page" do
+  	before { visit signup_path }
     it { should have_selector('title', text: full_title('Sign Up')) }
     it { should have_selector('h1', text: 'Sign Up') }
   end
+
+  describe "Profile Page" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit user_path(user) }
+  	it { should have_selector('title', text: user.name) }
+  	it { should have_selector('h1', text: user.name) }
+  end
+
+  describe "signup" do
+    let(:submit) { "Create my account" }
+
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",          with: "Example User"
+        fill_in "Email",         with: "user@example.com"
+        fill_in "Password",      with: "foobar"
+        fill_in "Password confirmation",  with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+
+  end
+
 end
